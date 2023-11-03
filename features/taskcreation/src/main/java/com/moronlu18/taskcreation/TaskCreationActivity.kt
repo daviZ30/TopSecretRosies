ba6b0c93ba94663576e3704e5316cd668a5a1862
jguegen <al.juan.guerra.gennich@iesportada.com>
2023-11-03 12:59:05 +0100

    Juanlu Task Version

---
+++ features/taskcreation/src/main/java/com/moronlu18/taskcreation/TaskCreationActivity.kt
@@ -0,0 +1,11 @@
+package com.moronlu18.taskcreation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskCreationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_creation)
+    }
+}
--- features/taskdetail/build.gradle.kts
+++ features/taskdetail/build.gradle.kts
@@ -20,24 +20,16 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
     implementation(project(":app"))
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
-    //Implementar la navegación por módulos
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
+    androidTestImplementation("androidx.annotation:annotation:1.7.0")
 }
