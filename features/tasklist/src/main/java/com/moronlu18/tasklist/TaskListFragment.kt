84b2b7b544a6d872cacf547c9a332d528f4db9b7
jguegen <al.juan.guerra.gennich@iesportada.com>
2023-11-09 21:42:50 +0100

    Mejoras en Task

--- features/tasklist/src/main/java/com/moronlu18/tasklist/TaskListFragment.kt
+++ features/tasklist/src/main/java/com/moronlu18/tasklist/TaskListFragment.kt
@@ -5,6 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+data class Task(val titulo: String, val cliente: String, val fecha: String, val hora: String)
+
+val tasks = listOf(
+    Task("Crear Tarea", "Juan Luis Guerra Gennich", "13/04/2002", "00:00"),
+    Task("Prueba List", "Alex Carnero Tapia", "09/11/2023", "00:00"),
+    Task("Exponer proyecto", "David Zambrana", "10/11/2002", "10:00"),
+    Task("Más pruebas", "Antonio Angel Salado Gomez", "01/01/2000", "00:01")
+)
 
 class TaskListFragment : Fragment() {
 
@@ -17,7 +29,12 @@ class TaskListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_task_list, container, false)
+
+        val rvTaskList = view.findViewById<RecyclerView>(R.id.rvTaskList)
+        rvTaskList.adapter = TaskListAdapter(tasks)
+        rvTaskList.layoutManager = LinearLayoutManager(context)
+        return view;
     }
 
 }
