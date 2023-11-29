313e6730d6d5e974544342b523096999b22d7668
Alex <al.alex.carnero.tapia@iesportada.org>
2023-11-29 09:05:16 +0100

    Dateset customer actualizado

--- domain/invoiceD/src/main/java/com/moronlu18/customer/repository/ProviderCustomer.kt
+++ domain/invoiceD/src/main/java/com/moronlu18/customer/repository/ProviderCustomer.kt
@@ -8,8 +8,8 @@ class ProviderCustomer private constructor() {
 
         private fun setUpDateSetCliente(): MutableList<Cliente> {
             var dataset : MutableList<Cliente> = ArrayList()
-            dataset.add(Cliente(1,"Alex","Carnero Tapia","carnetaadspjf@gmail.com",6824556414,"Málaga","Calle Leonora n46"))
-            dataset.add(Cliente(2,"Alex","Carnero Tapia","carnetaadspjf@gmail.com",6846556414,"Málaga","Calle Leonora n46"))
+            dataset.add(Cliente(1,"Juanlu","Cabrera Jimenez","carnetaadspjf@gmail.com",6824556414,"Málaga","Calle Leonora n46"))
+            dataset.add(Cliente(2,"Antonio","Urquiza FAlle","carnetaadspjf@gmail.com",6846556414,"Málaga","Calle Leonora n46"))
             dataset.add(Cliente(3,"Alex","Carnero Tapia","carnetaadspjf@gmail.com",6864646414,"Málaga","Calle Leonora n46"))
             return dataset
         }
