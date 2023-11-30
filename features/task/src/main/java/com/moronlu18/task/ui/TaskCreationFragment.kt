363647d2eca867a05c4a7703630b74b71b80f8c7
jguegen <al.juan.guerra.gennich@iesportada.com>
2023-11-30 19:28:27 +0100

    TaskCreation con Spinner y fecha inicial

--- features/task/src/main/java/com/moronlu18/task/ui/TaskCreationFragment.kt
+++ features/task/src/main/java/com/moronlu18/task/ui/TaskCreationFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import com.moronlu18.customer.entity.Cliente
 import com.moronlu18.customer.repository.ProviderCustomer
 import com.moronlu18.task.entity.Task
 import com.moronlu18.task.repository.ProviderTask
 import com.moronlu18.taskFragment.databinding.FragmentTaskCreationBinding
+import com.google.android.material.textfield.TextInputEditText
 import com.moronlu18.taskFragment.R
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 
 class TaskCreationFragment : Fragment() {
     var clientes = ProviderCustomer.datasetCustomer
@@ -20,19 +24,18 @@ class TaskCreationFragment : Fragment() {
     private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         var names : MutableList<String> = mutableListOf()
         for (cliente in clientes) {
-            names.add(cliente.getFullName())
+            names.add("${cliente.id}.-"+ cliente.getFullName())
         }
-        /*val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, names)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spTaskCreationCliente.adapter = adapter*/
+        binding.spTaskCreationCliente.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, names)
+        binding.tieTaskCreationDateStart.setText(SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(Date()))
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentTaskCreationBinding.inflate(inflater, container, false)
         return binding.root
     }
