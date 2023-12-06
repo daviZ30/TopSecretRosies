1fcd5c28d80b2f2bb836a3d9c95655d165b319dc
jguegen <al.juan.guerra.gennich@iesportada.com>
2023-12-06 21:18:17 +0100

    Cambios en viewmodel task

--- features/task/src/main/java/com/moronlu18/task/ui/TaskCreationFragment.kt
+++ features/task/src/main/java/com/moronlu18/task/ui/TaskCreationFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import com.moronlu18.customer.repository.ProviderCustomer
 import com.moronlu18.task.calendar.CalendarInvoice
@@ -14,15 +17,18 @@ import com.moronlu18.task.repository.ProviderTask
 import com.moronlu18.taskFragment.databinding.FragmentTaskCreationBinding
 import com.moronlu18.task.entity.TaskStatus
 import com.moronlu18.task.entity.TaskType
+import com.moronlu18.task.usecase.TaskViewModel
 
 
 class TaskCreationFragment : Fragment() {
     private val tasks: MutableList<Task> = ProviderTask.taskExample
     private val customer = ProviderCustomer.datasetCustomer
-    val c = CalendarInvoice()
+    private val c = CalendarInvoice()
 
     private var _binding: FragmentTaskCreationBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel : TaskViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -30,17 +36,28 @@ class TaskCreationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var names: MutableList<String> = mutableListOf()
-
-        //        var names: Map<Int,String> = mutableListOf()
+        //Añade los clientes al spinner y si no hay no puedes crear una tarea
         if (customer.isEmpty()) {
             names.add("<No Existen Clientes>")
+            binding.btnTaskCreationAdd.isEnabled = false
         } else {
+            names.add("--Selecciona un cliente--")
             for (cliente in customer) {
                 names.add("${cliente.id}.-" + cliente.getFullName())
             }
         }
         binding.spTaskCreationCliente.adapter =
             ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, names)
+
+        viewModel.getState().observe(viewLifecycleOwner){
+            when(it){
+                TaskState.TitleIsMandatoryError -> {}
+                TaskState.CustomerUnspecified -> {}
+                TaskState.IncorrectDateRangeError -> {}
+                TaskState.Success -> {}
+            }
+        }
+
 
         binding.tieTaskCreationDateStart.setText(c.getCurrentDate()) //Valor por defecto
         //PopUp de Calendario
@@ -67,7 +84,8 @@ class TaskCreationFragment : Fragment() {
             val createdDate = binding.tieTaskCreationDateStart.text.toString() ?: c.getCurrentDate()
             val endDate = binding.tieTaskCreationDateEnd.text.toString() ?: c.getCurrentDate()
             tasks.add(Task(idTask, idCliente, title, desc, nameCustomer!!, type, status, createdDate, endDate))
-            var bundle = Bundle()
+            Toast.makeText(requireContext(), "La tarea ha sido creada", Toast.LENGTH_SHORT).show()
+            val bundle = Bundle()
             parentFragmentManager.setFragmentResult("key", bundle)
             findNavController().popBackStack()
         }
@@ -79,6 +97,8 @@ class TaskCreationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentTaskCreationBinding.inflate(inflater, container, false)
+        binding.viewmodel = this.viewModel
+        binding.lifecycleOwner = this
         return binding.root
     }
     override fun onDestroyView() {
@@ -86,6 +106,9 @@ class TaskCreationFragment : Fragment() {
         _binding = null
     }
 
+    /**
+     * Devuelve el id del spinner
+     */
     fun getIdCliente(customer : String) : Int{
         var id : Int = customer.split(".").first().toInt()
         return id
--- features/task/src/main/java/com/moronlu18/task/usecase/TaskViewModel.kt
+++ features/task/src/main/java/com/moronlu18/task/usecase/TaskViewModel.kt
@@ -1,14 +1,26 @@
 package com.moronlu18.task.usecase
 
+import android.text.TextUtils
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.moronlu18.task.ui.TaskState
 
 class TaskViewModel : ViewModel() {
-    val title : String = ""
-    val createdDate : String = ""
-    val endDate: String = ""
+    val title = MutableLiveData<String>()
+    val customer = MutableLiveData<String>()
+    val createdDate = MutableLiveData<String>()
+    val endDate = MutableLiveData<String>()
+    private val state = MutableLiveData<TaskState>()
     fun validate(){
         when{
-
+            TextUtils.isEmpty(title.value)->state.value = TaskState.TitleIsMandatoryError
+            else ->{
+                state.value = TaskState.Success
+            }
         }
+    }
+    fun getState(): LiveData<TaskState> {
+        return state;
     }
 }
