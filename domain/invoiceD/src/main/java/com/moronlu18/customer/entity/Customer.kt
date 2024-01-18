b777d0bacbe8e821165bd31d977c5ccd636f0db3
Alex <al.alex.carnero.tapia@iesportada.org>
2024-01-18 10:09:23 +0100

    Interfaz Provider eliminado

--- domain/invoiceD/src/main/java/com/moronlu18/customer/entity/Customer.kt
+++ domain/invoiceD/src/main/java/com/moronlu18/customer/entity/Customer.kt
@@ -1,5 +1,6 @@
 package com.moronlu18.customer.entity
 
+import com.moronlu18.invoice.entity.Identificator
 import com.moronlu18.invoice.ui.firebase.Email
 
 data class Cliente(
@@ -10,7 +11,7 @@ data class Cliente(
     val telefono: String,
     val city: String,
     val direction: String
-) {
+): Identificator(id) {
     public fun getFullName(): String {
         return this.nombre + " " + this.apellidos
     }
