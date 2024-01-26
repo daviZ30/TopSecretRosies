6e9f1d7c91303b6ad4cbc1a4100042dd47211d30
jguegen <al.juan.guerra.gennich@iesportada.com>
2024-01-26 11:08:42 +0100

    Entity Task

--- domain/invoiceD/src/main/java/com/moronlu18/task/entity/Task.kt
+++ domain/invoiceD/src/main/java/com/moronlu18/task/entity/Task.kt
@@ -1,17 +1,28 @@
 package com.moronlu18.task.entity
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 import com.moronlu18.customer.entity.Cliente
 import com.moronlu18.invoice.entity.UniqueId
 
-
+@Entity(
+    tableName = "task", foreignKeys =
+    [ForeignKey(
+        entity = Cliente::class, parentColumns = arrayOf("id"), childColumns = arrayOf("customer"),
+        onDelete = ForeignKey.RESTRICT, onUpdate = ForeignKey.CASCADE
+    )]
+)
 data class Task(
+    @PrimaryKey
     val idTask: TaskId,
     var customer: Cliente,
     var title: String,
-    var description : String,
-    var type : TaskType,
+    var description: String,
+    var type: TaskType,
     var status: TaskStatus,
     var createdDate: String, //Instant
     var endDate: String //Instant
 )
+
 data class TaskId(override val value: Int) : UniqueId(value)
